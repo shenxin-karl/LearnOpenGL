9accc9074eddf2fe540fca43a5f4e99827b58ebb
shenxin <1683137709@qq.com>
2021-06-21 22:38:00 +0800

    commit glm and stb lib

---
+++ include/common.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include <array>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <memory>
+#include <type_traits>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+
+#include <stb_image.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
